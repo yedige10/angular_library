321cff64168b22a9f85a59bdfedc93e11d22c349
Daniyar <danchik90@gmail.com>
2018-10-15 14:04:40 +0600

    minor formatting

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
-import { Router, RouterModule,ActivatedRoute, NavigationExtras } from '@angular/router';
+import { Router, RouterModule, ActivatedRoute, NavigationExtras } from '@angular/router';
 
 
 @Component({
@@ -13,8 +13,8 @@ export class RegistrationComponent implements OnInit {
 
   user: User = new User();
   submitted = false;
-  x:User;
-  constructor(private userService:UserService,private router: Router,private route:ActivatedRoute) { }
+  x: User;
+  constructor(private userService: UserService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -26,45 +26,27 @@ export class RegistrationComponent implements OnInit {
 
 
   saveUser() {
-    if(this.user.stafforclient=="staff"){
-    this.userService.createUser(this.user)
-      .subscribe(data => {console.log(data);this.router.navigate(["adminpage/books"],{relativeTo:this.route})}, error => console.log(error));
-    }
-    else{
+    if (this.user.stafforclient == "staff") {
       this.userService.createUser(this.user)
-      .subscribe((data:any) => {console.log(data);this.x=data;
-        let navigationExtras: NavigationExtras = {
-          queryParams: {
-              "id":this.x.id,
+        .subscribe(data => { console.log(data); this.router.navigate(["adminpage/books"], { relativeTo: this.route }) }, error => console.log(error));
+    }
+    else {
+      this.userService.createUser(this.user)
+        .subscribe((data: any) => {
+          console.log(data);
+          this.x = data;
+          let navigationExtras: NavigationExtras = {
+            queryParams: {
+              "id": this.x.id,
               "username": this.x.username
-          }
-      };
-        this.router.navigate(["registration/userpage/books"],navigationExtras)}, error => console.log(error));
+            }
+          };
+          this.router.navigate(["registration/userpage/books"], navigationExtras)
+        }, error => console.log(error));
     }
-    }
-  
-  /*
-  saveClient() {
-    this.userService.createUserClient(this.user)
-      .subscribe(data => console.log(data), error => console.log(error));
-    this.user = new User();
   }
-  */
 
   onSubmit() {
     this.saveUser();
-    /*
-    if(this.user.stafforclient=="staff"){
-      
-      console.log("staff");  
-      this.saveStaff();
-      
-    }
-    else{
-      
-      console.log("client");
-      this.saveClient();
-    }
-    */
   }
-  }
+}
