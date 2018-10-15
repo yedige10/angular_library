04781f71d15963e9ea84568ab0584c8f56ad7b4c
Yedige <baltabekov.edige@mail.ru>
2018-10-15 14:00:32 +0600

    registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
-import { Router, RouterModule,ActivatedRoute } from '@angular/router';
+import { Router, RouterModule,ActivatedRoute, NavigationExtras } from '@angular/router';
 
 
 @Component({
@@ -13,7 +13,7 @@ export class RegistrationComponent implements OnInit {
 
   user: User = new User();
   submitted = false;
-
+  x:User;
   constructor(private userService:UserService,private router: Router,private route:ActivatedRoute) { }
 
   ngOnInit() {
@@ -32,7 +32,14 @@ export class RegistrationComponent implements OnInit {
     }
     else{
       this.userService.createUser(this.user)
-      .subscribe(data => {console.log(data);this.router.navigate(["userpage/books"],{relativeTo:this.route})}, error => console.log(error));
+      .subscribe((data:any) => {console.log(data);this.x=data;
+        let navigationExtras: NavigationExtras = {
+          queryParams: {
+              "id":this.x.id,
+              "username": this.x.username
+          }
+      };
+        this.router.navigate(["registration/userpage/books"],navigationExtras)}, error => console.log(error));
     }
     }
   
